62c80f7fc431ad39bbd0fb29f1f4896634d57e6f
Toby Davis <pencilcaseman@gmail.com>
2024-07-20 14:55:36 +0100

    Warming up the oven

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -86,12 +86,21 @@ pub mod pallet {
 		type WeightInfo: WeightInfo;
 	}
 
+	/// A struct that holds some useful values.
+	#[derive(
+		Encode, Decode, Default, PartialEq, Eq, Clone, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen,
+	)]
+	pub struct StorageType {
+		pub a: u32,
+		pub b: u32,
+	}
+
 	/// A storage item for this pallet.
 	///
 	/// In this template, we are declaring a storage item called `Something` that stores a single
 	/// `u32` value. Learn more about runtime storage here: <https://docs.substrate.io/build/runtime-storage/>
 	#[pallet::storage]
-	pub type Something<T> = StorageValue<_, u32>;
+	pub type Something<T> = StorageValue<_, StorageType>;
 
 	/// Events that functions in this pallet can emit.
 	///
@@ -109,7 +118,7 @@ pub mod pallet {
 		/// A user has successfully set a new value.
 		SomethingStored {
 			/// The new value set.
-			something: u32,
+			something: StorageType,
 			/// The account who set the new value.
 			who: T::AccountId,
 		},
@@ -152,7 +161,7 @@ pub mod pallet {
 		/// error if it isn't. Learn more about origins here: <https://docs.substrate.io/build/origins/>
 		#[pallet::call_index(0)]
 		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn do_something(origin: OriginFor<T>, something: StorageType) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			let who = ensure_signed(origin)?;
 
@@ -191,7 +200,12 @@ pub mod pallet {
 				Some(old) => {
 					// Increment the value read from storage. This will cause an error in the event
 					// of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					// let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+
+					let new_a = old.a.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					let new_b = old.b.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					let new = StorageType { a: new_a, b: new_b };
+
 					// Update the value in storage with the incremented result.
 					Something::<T>::put(new);
 					Ok(())
